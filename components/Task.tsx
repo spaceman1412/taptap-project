f3b08c0d56f4b60c85787db0fb7b54de374e08ba
Thien Nguyen <spacemannormal@gmail.com>
2024-04-24 14:04:54 +0700

    add date time picker

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,30 +1,158 @@
-import { View, ViewStyle, Text, TextStyle } from "react-native";
-import React from "react";
+import {
+  View,
+  ViewStyle,
+  Text,
+  TextStyle,
+  TextInput,
+  Button as NativeButton,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 import { colors } from "../themes/colors";
 import { SizedBox } from "./SizedBox";
 import { FontAwesome } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { Button } from "./Button";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export function Task(props) {
-  return (
-    <View style={$container}>
-      <CheckBox />
+  return <OpenTask />;
+}
 
+const CloseTask = () => {
+  return (
+    <View style={$openContainer}>
+      <CheckBox />
       <View style={$taskContainer}>
         <Text style={$text}>Task 1</Text>
-
         <Text style={$priorityText}>Ưu tiên cao</Text>
       </View>
       <View style={$endContainer}>
         <FontAwesome name="pencil" size={24} color="black" />
-
         <Text style={$dateText}>Còn 2 ngày</Text>
       </View>
     </View>
   );
-}
+};
+
+const getFormattedDate = (date: Date) => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  // This arrangement can be altered based on how we want the date's format to appear.
+  let currentDate = `${day}/${month}/${year}`;
+
+  return currentDate;
+};
+
+const OpenTask = () => {
+  const [text, onChangeText] = useState("Task 1");
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showMode = () => {
+    setShow(true);
+  };
+
+  return (
+    <View style={$closeContainer}>
+      <View
+        style={{
+          alignSelf: "flex-end",
+          justifyContent: "flex-end",
+          flexDirection: "row",
+        }}
+      >
+        <Feather name="trash-2" size={24} color="black" />
+        <SizedBox width={5} />
+        <Text style={$deleteText}>Xóa</Text>
+      </View>
+      <SizedBox height={8} />
+      <TextInput
+        value={text}
+        onChangeText={onChangeText}
+        style={{
+          borderBottomWidth: 1,
+          fontSize: 16,
+          fontWeight: "500",
+          paddingVertical: 8,
+        }}
+      />
+
+      <SizedBox height={22} />
+      <TouchableOpacity
+        onPress={showMode}
+        style={{ flexDirection: "row", justifyContent: "space-between" }}
+      >
+        <Text style={$text}>Thời hạn</Text>
+        <Text>{getFormattedDate(date)}</Text>
+      </TouchableOpacity>
+
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={"date"}
+          is24Hour={true}
+          onChange={onChange}
+        />
+      )}
+
+      <SizedBox height={8} />
+
+      <View style={{ height: 1, backgroundColor: colors.gray }} />
+
+      <SizedBox height={22} />
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Text style={$text}>Mức độ ưu tiên</Text>
+        <Text>Cao</Text>
+      </View>
+      <SizedBox height={8} />
+
+      <View style={{ height: 1, backgroundColor: colors.gray }} />
+
+      <SizedBox height={32} />
+
+      <TouchableOpacity style={$confirmButton}>
+        <Text style={{ color: colors.white }}>Xong</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const CheckBox = () => {
   return <View style={$checkBox} />;
+};
+
+const $confirmButton: ViewStyle = {
+  width: 85,
+  height: 28,
+  borderRadius: 20,
+  backgroundColor: colors.green,
+  justifyContent: "center",
+  alignItems: "center",
+  alignSelf: "center",
+};
+
+const $deleteText: TextStyle = {
+  fontSize: 14,
+  fontWeight: "400",
+};
+
+const $closeContainer: ViewStyle = {
+  width: 327,
+  height: 299,
+  backgroundColor: colors.white,
+  borderRadius: 15,
+  paddingHorizontal: 24,
+  paddingVertical: 32,
 };
 
 const $dateText: TextStyle = {
@@ -58,13 +186,13 @@ const $checkBox: ViewStyle = {
   backgroundColor: colors.gray,
 };
 
-const $container: ViewStyle = {
+const $openContainer: ViewStyle = {
   width: 327,
   height: 114,
   backgroundColor: colors.white,
   borderRadius: 15,
   flexDirection: "row",
-  paddingHorizontal: 16,
+  paddingHorizontal: 24,
   paddingVertical: 32,
 };
 
